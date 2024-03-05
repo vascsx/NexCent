56ae5f41420756729248f5a2840223b8280af9ee
vascsx <vasczn@gmail.com>
2024-03-05 16:26:00 -0300

    initial commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+
+@Component({
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    imports: [
+      HeaderComponent
+    ]
+})
+export class HomeComponent {
+
+}
