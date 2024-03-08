e5b4ee6017f16126b207c4514ac2f4e7918fa725
vascsx <vasczn@gmail.com>
2024-03-08 15:10:04 -0300

    feat: create atividades

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -9,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class ButtonComponent {
 
+
+  @Input() text!: string;
+ 
 }
